3ca763354b6bcb559ec4b6d1c9b5f34de3b17609
the-arcade-01 <aashisharcade@gmail.com>
2020-04-08 18:21:13 +0530

    api resolved

---
+++ Dictionary/api.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+app_id = "050970e7"
+app_key = "863d4f01d8a7e5b8aa359bbe9c9436aa"
+language = "en-gb"
+word_id = "example"
+
+url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
+r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+
+results = r.json()['results']
+# print(json.dumps(results,indent=2,sort_keys=True))
+
+definition = results[0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
+example = results[0]['lexicalEntries'][0]['entries'][0]['senses'][0]['examples'][0]['text']
+synonyms = results[0]['lexicalEntries'][0]['entries'][0]['senses'][0]['synonyms']
+pronunciation = results[0]['lexicalEntries'][0]['pronunciations'][0]['audioFile']
+
+print(definition)
+print(example)
+print(pronunciation)
+for item in synonyms:
+    print(item['text'])
